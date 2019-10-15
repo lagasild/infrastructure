4f82d9a5b7c4e900f944a1fc59fc0b06a57467c5
olaven <olav@sundfoer.com>
2019-10-16 00:57:09 +0200

    changed URL and name of statuscake test

--- monitoring.tf
+++ monitoring.tf
@@ -4,8 +4,8 @@ provider "statuscake" {
 }
 
 resource "statuscake_test" "prod" {
-    website_name = "app-prod"
-    website_url = "${module.heroku_module.production_url}"
+    website_name = "app-test" #test makes sense for this exam, usually this would be prod.
+    website_url = "${module.heroku_module.production_url}/devices"
     test_type = "HTTP"
     check_rate = "300"
     contact_group = ["Default Contact Group"]

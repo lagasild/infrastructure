ce6ef5a335d744d0f7938eb6a47704b4de9ca8b6
olaven <olav@sundfoer.com>
2019-10-15 15:56:44 +0200

    added terraform backend and travis automation

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "s3" {
+        bucket = "pgr301bucket"
+        key    = "lagasild/terraform.tfstate"
+        region = "eu-north-1"
+    }
+}
+
 module heroku_apps {
     source = "./module"
     name = "exam" 

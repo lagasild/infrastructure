1850b7f9bd18ca9ebc6fd9073569c5d1299fbbae
lagasild <lagasild@protonmail.com>
2019-10-15 22:24:09 +0200

    explicitly stating heroku-provider from parent

--- deployment.tf
+++ deployment.tf
@@ -1,7 +1,12 @@
-module "heroku" {
+provider "heroku" {}
+
+module "heroku_module" {
     source = "./heroku_module"
     name = "devops-exam" 
     buildpacks = [    
         "heroku/java"
     ]
+    providers = {
+        heroku = heroku
+    }
 }

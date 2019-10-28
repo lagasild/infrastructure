c924ceee533bccb296d7db9a67cc5a426242d719
lagasild <lagasild@protonmail.com>
2019-10-28 16:24:56 +0100

    terraform adds config vars for lgzio

--- main.tf
+++ main.tf
@@ -6,10 +6,14 @@ terraform {
     }
 }
 
+variable "logz_token" {}
+variable "logz_url" {}
 module "heroku_module" {
     source = "./heroku_module"
     name = "devops-exam"
     buildpacks = [
         "heroku/java"
     ]
+    logz_token = "${var.logz_token}"
+    logz_url = "${var.logz_url}"
 }
